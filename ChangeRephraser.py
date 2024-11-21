03d12398383bc4a0ef3933a0f114d147adc1946e
Mohammed Mostafa <mohammed.mostafa@anaplan.com>
2024-11-20 20:31:37 -0500

    changes

--- ChangeRephraser.py
+++ ChangeRephraser.py
@@ -1,12 +1,12 @@
 from langchain.output_parsers import ResponseSchema
 from langchain_community.llms.ollama import Ollama
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
 
 class ChangeRephraser(object):
-    LLAMA = False
+    LLAMA = True
     DEBUG = False
 
     def __init__(self):
@@ -23,17 +23,38 @@ class ChangeRephraser(object):
              subsection for each file that is not a test file titled with the file name each of these sub sections will 
              contain a bullet list summarizing the important changes in this file and their reason do not include more than 3 bullets.
              The Test changes section will be included only if there are test files and its contents will follow the same format as the Logic Section.
-             The description should be in Markdown format. do include any thing in the response other than markdown.
+             The description should be in Markdown format. do not include any thing in the response other than markdown.
              """),
             ("user", "{input}")
         ]
+
+        template_str = """\
+        describe the changes done to a software project in github, the description of 
+             these changes should be in Markdown format to be used as a Pull Request description.
+             The description should follow the provided description sample if it is not empty otherwise it should have a
+             title of up to 6 words summarizing the changes, followed by up to 2 sections one tilted "Logic changes" and
+             one titled "Test changes" the logic changes section will contain a subsection for each file that is not a 
+             test file titled with the file name each of these sub sections will contain a bullet list summarizing the 
+             important changes in this file and their reason do not include more than 3 bullets. The Test changes 
+             section will be included only if there are test files and its contents will follow the same format as the 
+             Logic Section. The description should be in Markdown format. do not include any thing in the response 
+             other than markdown.
+             The changes are: {change}
+             Description Sample: {sample}
+        """
+        self.rephrase_template = PromptTemplate(input_variables=["change","sample"],
+                                           template=template_str)
+
         self.prompt_template = ChatPromptTemplate.from_messages(self.prompt)
         self.chain = self.prompt_template | self.chat | StrOutputParser()
 
     def convert(self, text):
         if self.DEBUG:
             print(self.prompt_template1)
-        out = self.chain.invoke(text)
+        #out = self.chain.invoke(text)
+        formated_template = self.rephrase_template.format(change= text, sample = "")
+        out = self.chat.invoke(formated_template)
+        print(formated_template)
         if self.DEBUG:
             for key, value in out:
                 print(key + " ==> " + str(value))
